20c286688c386b8d3f93e5a667976f711d286997
Hung Nguyen <hung@redhotminute.com.au>
2019-04-11 14:46:38 +1000

    Fix issue where UI View can't be updated from a thread other than Main thread

--- app/src/main/java/com/example/kotlin_mvp_login/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/kotlin_mvp_login/login/view/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlin_mvp_login.login.view
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.example.kotlin_mvp_login.R
 import com.example.kotlin_mvp_login.list.view.ListActivity
@@ -12,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
 
     private val loginPresenter = LoginPresenterCompl(this)
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +22,9 @@ class LoginActivity : AppCompatActivity(), ILoginView, View.OnClickListener {
 
     override fun onLoginResult(result: Boolean?) {
 
-        loginPresenter.setProgressBarVisibility(View.GONE)
+        handler.post {
+            loginPresenter.setProgressBarVisibility(View.GONE)
+        }
 
         if (result == true) {
             val intent = Intent(this, ListActivity::class.java)
